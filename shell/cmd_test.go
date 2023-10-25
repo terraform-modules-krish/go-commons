3366fb346b53f8411a79bae88e9e8d4065ceb0bf
repo-copier <info@gruntwork.io>
2023-10-24 18:55:31 -0600

    Resolve references

--- shell/cmd_test.go
+++ shell/cmd_test.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/gruntwork-io/gruntwork-cli/logging"
+	gruntwork-cli "github.com/terraform-modules-krish/go-commons/logging"
 	"github.com/stretchr/testify/assert"
 )
 
